7c2d8cc15f244e0787d296b62f1f58c8eafddc8e
Blake Georgeff <georgeffb@hotmail.com>
2021-09-11 17:14:24 +0800

    changing cookies

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.COOKIE_SECRET,
+  secret: 'hello123',
   cookie: {},
   resave: false,
   saveUninitialized: true,
